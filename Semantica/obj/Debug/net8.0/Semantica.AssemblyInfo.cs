a73db723f9f4375cdce657442b7f2e5268f67b12
Edgar Oviedo <eoviedocuevas.2003@gmail.com>
2025-02-25 14:24:16 -0600

    Hola

--- Semantica/obj/Debug/net8.0/Semantica.AssemblyInfo.cs
+++ Semantica/obj/Debug/net8.0/Semantica.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Semantica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2428e741dd4400b3eb6f5e90717329b90f36908a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1865b3a3268e159bbf4a11ffd1f611dad1013387")]
 [assembly: System.Reflection.AssemblyProductAttribute("Semantica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Semantica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
